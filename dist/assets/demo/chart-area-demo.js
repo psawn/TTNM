accffd5f12cb93ef4ef0c915423819b3fcccece0
psawn <psawn0972055909@yahoo.com.vn>
2020-12-18 10:01:21 +0700

    Update giao diện

--- dist/assets/demo/chart-area-demo.js
+++ dist/assets/demo/chart-area-demo.js
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [100, 301, 262, 183, 182, 286, 312, 332, 258, 241, 326, 319, 384],
     }],
   },
   options: {
@@ -39,7 +39,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 40000,
+          max: 400,
           maxTicksLimit: 5
         },
         gridLines: {
